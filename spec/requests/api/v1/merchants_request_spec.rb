fbcf08757d0c8f25056d8ac07b709c5caf70c18f
Kevin McGrevey <niv3kmcg@gmail.com>
2020-05-17 14:10:19 -0600

    Adds merchant show API functionality

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -13,4 +13,16 @@ describe "Merchants API" do
     expect(merchants["data"].count).to eq(3)
     expect(merchants["data"].first["attributes"]["name"].class).to eq(String)
   end
+
+  it "can get one item by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+   
+    expect(merchant["data"]["id"]).to eq(id.to_s)
+  end
 end
