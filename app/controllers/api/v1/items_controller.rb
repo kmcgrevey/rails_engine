0180c340cf4ca18dc5d5cfad4aa1c7061f94e5bb
Kevin McGrevey <niv3kmcg@gmail.com>
2020-05-18 21:01:23 -0600

    Adds item API crud functions with testing

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,27 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.all)
   end
 
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    render json: ItemSerializer.new(Item.create(body_params))
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(body_params)
+    render json: ItemSerializer.new(item)
+  end
+
+  def destroy
+    render json: ItemSerializer.new(Item.destroy(params[:id]))
+  end
+
+  private
+
+    def body_params
+      params.permit(:name, :description, :unit_price, :merchant_id)
+    end
 end
