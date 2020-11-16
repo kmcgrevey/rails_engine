0e61f1c783eccdd7dd1542f1404b8c2db60a0d23
Kevin McGrevey <niv3kmcg@gmail.com>
2020-11-16 09:08:27 -0700

    Adds Merchant test to find revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,5 +4,25 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
     it { should have_many :invoices }
+    it { should have_many(:invoice_items).through(:invoices)}
+    it { should have_many(:transactions).through(:invoices)}
+  end
+
+  describe 'class methods' do
+    before(:each) do
+      inv_items = create_list(:invoice_item, 4)
+      inv_items.each do |item|
+        item.invoice.transactions.create(
+          result: 'success')
+      end
+    end
+    
+    it '.revenue' do
+      inv_item = InvoiceItem.first
+      merchant = inv_item.invoice.merchant
+      rev = Merchant.revenue(merchant.id)
+
+      expect(rev).to eq(inv_item.quantity * inv_item.unit_price)
+    end
   end
 end
