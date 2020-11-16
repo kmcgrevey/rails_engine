f8cc4d40a7879adcb835670de88710a63405b511
Kevin McGrevey <niv3kmcg@gmail.com>
2020-11-16 09:13:47 -0700

    Modifies model relationships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 end
